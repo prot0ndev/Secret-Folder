4ec51854757106e43592cfbbdf6cd50db9a8d180
Botiwa <76008188+prot0ndev@users.noreply.github.com>
2025-10-17 20:17:08 +0300

    Add files via upload

---
+++ key.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
+if NOT EXIST Private goto MDPrivate
+:CONFIRM
+echo Are you sure to lock this folder? (Y/N)
+set/p "cho=> "
+if %cho%==Y goto LOCK
+if %cho%==y goto LOCK
+if %cho%==n goto END
+if %cho%==N goto END
+echo Invalid choice.
+goto CONFIRM
+:LOCK
+ren Private "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+echo Folder locked
+goto End
+:UNLOCK
+echo Enter your passkey to unlock the secure folder: 
+set/p "pass=> "
+REM You can set your password from below this comment
+if NOT %pass%== ballsofsigma123 goto FAIL
+REM You can set your password from above this comment
+attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Private
+echo Folder Unlocked successfully
+goto End
+:FAIL
+echo Invalid password
+goto end
+:MDPrivate
+md Private
+echo Private created successfully
+goto End
+:Ends
